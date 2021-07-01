aef4c28139f5f2408bcb02e1f1af445d31e7a524
Amio Sarker <amiosarker@Amios-Air.fios-router.home>
2021-07-01 18:02:34 -0400

    I made a character in aseprite and made it rotate and play animations. Also I Spruced up the level with better graphics.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,6 +8,9 @@ public class Movement : MonoBehaviour
     public float Jumpforce = 1f;
     private Rigidbody2D rb;
     public float lockPos = 0f;
+    public float rotateref = 0f;
+    public float flip = 0f;
+    public Animator anim;
  
     // Start is called before the first frame update
     void Start()
@@ -18,13 +21,31 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        transform.rotation = Quaternion.Euler (lockPos ,lockPos ,lockPos);
+         anim.SetFloat("speed", Mathf.Abs(rotateref));
+        rotateref = Input.GetAxis("Horizontal");
+        transform.rotation = Quaternion.Euler (lockPos ,flip ,lockPos);
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement,0 ,0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump")&& Mathf.Abs(rb.velocity.y)< 0.002f)
+        if (Input.GetButtonDown("Jump")&& Mathf.Abs(rb.velocity.y)< 0.5f)
         {
+         
+         anim.SetBool("isjump", true);
          rb.AddForce(new Vector2(0, Jumpforce), ForceMode2D.Impulse);
+         
         }
+        if (rotateref < 0)
+       {
+         flip = 180f;
+       }
+        if(rotateref > 0)
+       {
+        flip = 0;
+       }
     }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+     anim.SetBool("isjump", false);
+    }
+
 }
